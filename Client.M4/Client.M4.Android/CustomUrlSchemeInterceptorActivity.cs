b0f3d421e1f9798ec35e053773001ea67dcd1f39
Egon C. Rasmussen <ecr@eucsyd.dk>
2020-02-13 13:43:13 +0100

    ok

---
+++ Client.M4/Client.M4.Android/CustomUrlSchemeInterceptorActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Content;
+using System;
+
+namespace Client.M4.Droid
+{
+    [Activity(Label = "CustomUrlSchemeInterceptorActivity", NoHistory = true, LaunchMode = LaunchMode.SingleTop)]
+    [IntentFilter(
+     new[] { Intent.ActionView },
+     Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+     DataSchemes = new[] { "com.pluralsight.windows" },
+     DataPath = "/callback")]
+    public class CustomUrlSchemeInterceptorActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Convert Android.Net.Url to Uri
+            var uri = new Uri(Intent.Data.ToString());
+
+            // Load redirectUrl page
+            //AuthenticationState.Authenticator.OnPageLoading(uri);
+
+            
+
+           // Finish();
+        }
+    }
+}
--- Client.M4/Client.M4.Android/MainActivity.cs
+++ Client.M4/Client.M4.Android/MainActivity.cs
@@ -28,6 +28,11 @@ namespace Client.M4.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            
         }
     }
 }
+
+
+//Name = "com.companyname.client.m4.MainActivity", 
